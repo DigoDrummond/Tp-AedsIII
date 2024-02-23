37ee409a6674b688fb3907dcf9ecc9a0247d9d6e
Rodrigo Drummond <rodrigodm2810@gmail.com>
2024-02-22 21:17:32 -0300

    Começo menu

--- TP.java
+++ TP.java
@@ -8,9 +8,11 @@ import java.util.Scanner;
 public class TP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         FileOutputStream arqByte;
         DataOutputStream dos;
 
+        //Escrevendo dados do .csv no arquivo .db
         try {
             FileReader fileReader = new FileReader("netflix.csv");
             BufferedReader arq = new BufferedReader(fileReader);
@@ -36,7 +38,34 @@ public class TP {
         } catch (Exception e) {
             System.out.println(e.getMessage() + "\n" + e.getLocalizedMessage());
         }
+        
+        System.out.println("#--------------- MENU ---------------#");
+        System.out.println(" Selecione uma das opções: ");
+        System.out.println("1)Adicionar novo registro na base de dados.\n2)Ler registro da base.\n3)Atualizar registro.\n4)Deletar registro. ");
+        
+
+
+
+
 
         sc.close();
     }
+    /* 
+    public static void create(Netflix netflix){
+
+    }
+
+    public static Netflix read(int id){
+
+    }
+
+    public static boolean update(Netflix netflix){
+
+    }
+
+    public static void delete(int id){
+
+    }
+    */
+
 }
